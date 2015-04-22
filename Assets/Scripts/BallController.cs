ebfa2c8ec8fd36bddc83a5462e57838170cee56b
BloodOfTheWolf <kevin.v.hill@gmail.com>
2015-04-21 19:57:47 -0700

    [RollABall]  - Initial Commit.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour 
+{
+
+	public float moveSpeed;
+	public float jumpSpeed;
+	bool isGrounded = true;
+
+	void Update () 
+	{
+		float x = Input.GetAxis ("Horizontal")* moveSpeed * Time.deltaTime;
+		float z = Input.GetAxis ("Vertical")* moveSpeed * Time.deltaTime;
+
+		Vector3 force = new Vector3 (x , 0f , z);
+			
+		// if the jump button is pressed and isGrounded is true, jump once and change isGrounded to false
+		if (Input.GetButton ("Jump")&& isGrounded) 
+		{
+			force.y = jumpSpeed;
+			isGrounded = false;
+		}
+
+		rigidbody.AddForce (force);
+	}
+
+	// if the tag of the collision object is JumpableSurface, reset the isGrounded condition to true
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.collider.CompareTag ("JumpableSurface")) 
+		{
+			isGrounded = true;
+		}
+	}
+
+	// if the tag of the trigger object the script holder enters is CoinTag, make it inactive and increase the score	
+	void OnTriggerEnter (Collider other) 
+		{
+		if (other.CompareTag("CoinTag")) 
+		{
+			other.gameObject.SetActive (false);
+			ScoreTracker.Instance.AddScore(100);
+		}
+	}
+}
---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+	public GameObject ball;
+	Vector3 offset; //The camera position minus the ball position gives the distance between the ball and the camera as a Vector3, this will be our quantifier for that
+
+	void Start () 
+	{
+		offset = transform.position - ball.transform.position; //my current location (the camera) minus the ball's position is equal to the offset
+	}
+	
+	void Update () 
+	{
+		transform.position = ball.transform.position + offset; //because we get the offset by subtracting the camera location from the ball location, we can tell the program
+															   //where we want the ball by saying the camera location is equal to the ball location plus the offset
+	}
+}
